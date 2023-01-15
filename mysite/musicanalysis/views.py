3afa780458b4e28be45cfb81f50a5fc1651adb51
yoshimochi <olive.oil6319@gmail.com>
2023-01-16 00:23:52 +0900

    (ADD)プレイリスURL送信用フォームの追加

--- mysite/musicanalysis/views.py
+++ mysite/musicanalysis/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .forms import SearchPassForm
+
+
 # Create your views here.
 def top(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        search_pass = request.POST["search_pass"]
+        form = SearchPassForm(initial={'search_pass': search_pass})
+    else:
+        return render(request, 'index.html')
